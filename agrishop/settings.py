d96226a6c8e095ff449c2dcc1b662b992653d833
Ali Bazoubandi <alibazoubandi@gmail.com>
2026-08-08 10:24:07 +0330

    fifteenth commit, fix railway errors for django-jalali library

--- agrishop/settings.py
+++ agrishop/settings.py
@@ -19,7 +19,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
-    'django_jalali',
     # our apps
     'shop',
     'blog',
